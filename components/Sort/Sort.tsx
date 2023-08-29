b371327f10205b8e24754c87ce5d94bd826bc87d
AsEqC <aseq04@gmail.com>
2023-08-29 09:26:55 +0600

    Accessibility - keyboard for sorting

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -2,11 +2,11 @@ import { SortEnum, SortProps } from "./Sort.props";
 import styles from "./Sort.module.css";
 import cn from "classnames";
 import { SortIcon } from "@/icon-components/SortIcon";
-
+import { KeyboardEvent } from "react";
 export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
           [styles.active]: sort === SortEnum.Rating,
@@ -14,8 +14,8 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
       >
         <SortIcon className={styles.sortIcon} />
         По рейтингу
-      </span>
-      <span
+      </button>
+      <button
         onClick={() => setSort(SortEnum.Price)}
         className={cn({
           [styles.active]: sort === SortEnum.Price,
@@ -23,7 +23,7 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
       >
         <SortIcon className={styles.sortIcon} />
         По цене
-      </span>
+      </button>
     </div>
   );
 };
